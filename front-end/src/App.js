09ec35cbeea583c3252609249113ca80544f56c5
Yogue1997 <yousyogue@gmail.com>
2023-02-14 17:51:51 -0600

    complete

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
+import Login from './aut/Login';
+import "react-toastify/dist/ReactToastify.css";
+import Home from '../src/components/Home'
+import NavBarStatus from './components/NavBars/NavBarStatus';
+import Notification from './components/NavBars/Notification';
+import NavbarLoggedIn from './components/NavBars/NavBarLoggedIn';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavbarLoggedIn />
+      {/* <NavBarStatus/> */}
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </div>
+      <ToastContainer />
+    </BrowserRouter>
   );
 }
 
